08a815fc3a6c2746aeaadd3246cb3e8e98b5d241
korotkovak <kristinakorotkova059@gmail.com>
2023-01-12 15:08:24 +0800

    Initial Commit

---
+++ Albums screen of the Photos app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Albums screen of the Photos app
+//
+//  Created by Kristina Korotkova on 12/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
