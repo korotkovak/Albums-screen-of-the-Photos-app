7c6002f48100b1fc27685277f4ce657a4d77b85f
korotkovak <kristinakorotkova059@gmail.com>
2023-01-12 15:17:39 +0800

    Created AlbumsScreenViewController

---
+++ Albums screen of the Photos app/Sources/AlbumsScreenViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AlbumsScreenViewController.swift
+//  Albums screen of the Photos app
+//
+//  Created by Kristina Korotkova on 12/01/23.
+//
+
+import UIKit
+
+class AlbumsScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
