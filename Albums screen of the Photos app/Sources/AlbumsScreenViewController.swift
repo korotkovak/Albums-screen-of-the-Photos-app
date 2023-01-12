557f8c3eb4d59805b92b664a5761b416fd323b59
korotkovak <kristinakorotkova059@gmail.com>
2023-01-12 16:14:30 +0800

    Created UICollectionView

--- Albums screen of the Photos app/Sources/AlbumsScreenViewController.swift
+++ Albums screen of the Photos app/Sources/AlbumsScreenViewController.swift
@@ -9,9 +9,53 @@ import UIKit
 
 class AlbumsScreenViewController: UIViewController {
 
+    // MARK: Outlets
+
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+        //        collectionView.delegate = self
+        //        collectionView.dataSource = self
+        return collectionView
+    }()
+
+    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupView()
+        setupHierarchy()
+        setupLayout()
     }
 
+    // MARK: - Setup
+
+    private func setupView() {
+        title = "Albums"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+    private func setupHierarchy() {
+        view.addSubview(collectionView)
+    }
+
+    private func setupLayout() {
+        collectionView.snp.makeConstraints { make in
+            make.left.top.right.bottom.equalTo(view)
+        }
+    }
+
+    private func createLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout { section, _ in
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.2),
+                                                  heightDimension: .fractionalWidth(0.2))
+            let layoutItem = NSCollectionLayoutItem(layoutSize: itemSize)
+
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                                   heightDimension: .fractionalHeight(1))
+            let layoutGroup = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: layoutItem, count: 5)
+
+            let sectionLayout = NSCollectionLayoutSection(group: layoutGroup)
+            return sectionLayout
+        }
+    }
 }
